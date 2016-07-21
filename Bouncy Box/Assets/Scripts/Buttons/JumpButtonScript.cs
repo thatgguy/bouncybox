292bbfc517c4d1639560dcfeb7de4eeb36f552ea
thatgguy <anthonydelsordo@yahoo.com>
2016-07-21 17:11:39 -0400

    mobile controls added

---
+++ Bouncy Box/Assets/Scripts/Buttons/JumpButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class JumpButtonScript : MonoBehaviour {
+
+	GameObject playerObj;
+	// playerObjScript;
+
+	// Use this for initialization
+	void Start () {
+		playerObj = GameObject.FindGameObjectWithTag ("Player");
+		//	playerObjScript = playerObj.GetComponent<PlayerMovement> ();
+	}
+	/*
+	void Update () {
+		
+	}*/
+
+	public void PlayerJump() {
+		playerObj.GetComponent<PlayerMovement>().Jump();
+	}
+
+	public void StopJump() {
+		playerObj.GetComponent<PlayerMovement> ().StopJump ();
+	}
+}
---
+++ Bouncy Box/Assets/Scripts/Buttons/LeftButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LeftButtonScript : MonoBehaviour {
+
+	GameObject playerObj;
+	// playerObjScript;
+
+	// Use this for initialization
+	void Start () {
+		playerObj = GameObject.FindGameObjectWithTag ("Player");
+	//	playerObjScript = playerObj.GetComponent<PlayerMovement> ();
+	}
+	
+	public void MoveLeft() {
+		playerObj.GetComponent<PlayerMovement>().MoveLeft ();
+	}
+}
