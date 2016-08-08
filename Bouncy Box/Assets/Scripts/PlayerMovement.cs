ec9e878ba7f6b0e029f13b0be7765e406a6674c7
bobby <bobbyvklein@gmail.com>
2016-08-08 17:13:02 -0400

    fixed and tightened controls.

--- Bouncy Box/Assets/Scripts/PlayerMovement.cs
+++ Bouncy Box/Assets/Scripts/PlayerMovement.cs
@@ -21,7 +21,7 @@ public class PlayerMovement : MonoBehaviour {
 */
 		rb = GetComponent<Rigidbody2D> ();
 		jumpSpeed = 18; //amount of force added when jumping
-		moveSpeed = 10; //speed when moving left/right
+		moveSpeed = 15; //speed when moving left/right
 		jumpTimer = .2f; //amount of time the player can hold the jump button.5
 	}
 	/*
