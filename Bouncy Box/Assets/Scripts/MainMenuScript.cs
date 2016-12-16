9fb050534c19d80710e65b7db22e769627b5b304
thatgguy <anthonydelsordo@yahoo.com>
2016-12-16 10:50:12 -0500

    fixed some bullshit

---
+++ Bouncy Box/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public Button startButton;
+	//public Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+		Button strtBtn = startButton.GetComponent<Button> ();
+		strtBtn.onClick.AddListener (StartButtonClick);
+
+		//Button xtBtn = exitButton.GetComponent<Button> ();
+		//xtBtn.onClick.AddListener (ExitButtonClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void StartButtonClick() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	/*void ExitButtonClick() {
+		
+	}*/
+}
